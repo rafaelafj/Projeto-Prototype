a056a9b26e42c652c3707413c577c100d5a6dc5b
rafaelafj <54038023+rafaelafj@users.noreply.github.com>
2022-04-22 12:36:35 -0300

    Add files via upload

---
+++ Prototype/src/Ford.java
@@ -0,0 +1,13 @@
+
+public class Ford extends Carro {
+    public Ford(String m)
+    {
+      nomeModelo = m;
+    }
+
+    @Override
+    public Carro clone() throws CloneNotSupportedException
+    {
+      return (Ford)super.clone();
+    }
+}
